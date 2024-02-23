8f95836ea6819df54d09a2954602d855b040ac2f
DishaSharma11 <154111933+DishaSharma11@users.noreply.github.com>
2024-02-23 12:44:53 +0530

    Create uniqueInSortedArray.java

---
+++ uniqueInSortedArray.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static int removeDuplicates(int[] arr,int n) {
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]!=arr[i-1]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
