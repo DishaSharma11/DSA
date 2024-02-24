edc73b2781b34caaef09da30211136c21aaadaa0
DishaSharma11 <154111933+DishaSharma11@users.noreply.github.com>
2024-02-24 15:10:24 +0530

    rotateArrayKtimes.java

---
+++ rotateArrayKtimes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Solution {
+    public static void reverse(ArrayList<Integer> arr,int i,int j){
+        while(i<=j){
+            int temp=arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,temp);
+            i++;
+            j--;
+        }
+    }
+	public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
+        int n=arr.size();
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr,0,n-1);
+        return arr;
+    }
+}
